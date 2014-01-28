5492333d6f80d607164a840d26df7bcdcad94a8e
chuckis <chuckis94@gmail.com>
2014-01-28 18:22:51 +0200

    modified tests.py

--- note/tests.py
+++ note/tests.py
@@ -34,45 +34,16 @@ def test_note_list_view(self):
 import unittest
 from selenium import webdriver
 
-class TestSignup(unittest.TestCase):
-
- def setUp(self):
-     self.driver = webdriver.Firefox()
-
- def test_signup_fire(self):
-     self.driver.get("http://localhost:8000/add/")
-     self.driver.find_element_by_id('id_title').send_keys("test title")
-     self.driver.find_element_by_id('id_body').send_keys("test body")
-     self.driver.find_element_by_id('submit').click()
-     self.assertIn("http://localhost:8000/", self.driver.current_url)
-
- def tearDown(self):
-     self.driver.quit
-
 if __name__ == '__main__':
  unittest.main()
-
-# api
-
-#from tastypie.test import ResourceTestCase
-
-#class EntryResourceTest(ResourceTestCase):
-
-    #def test_get_api_json(self):
-        #resp = self.api_client.get('/api/note/', format='json')
-        #self.assertValidJSONResponse(resp)
-
-    #def test_get_api_xml(self):
-        #resp = self.api_client.get('/api/note/', format='xml')
-        #self.assertValidXMLResponse(resp)
 
 # model mommy
 
 from model_mommy import mommy
 
-class WhateverTestMommy(TestCase):
+class NoteTestMommy(TestCase):
 
-    def test_whatever_creation_mommy(self):
+    def test_note_creation_mommy(self):
         n = mommy.make(Note)
         self.assertTrue(isinstance(n, Note))
         self.assertEqual(n.__unicode__(), n.title)
